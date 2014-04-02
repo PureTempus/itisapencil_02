24f64ef63b2204c5398795832ab39e685c6d5da3
PureTempus <simon_dowdney@cwgsy.net>
2014-04-02 19:16:59 +0100

    updated the initialise function

--- phonegap-workshop-master/js/main.js
+++ phonegap-workshop-master/js/main.js
@@ -23,7 +23,10 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        var self = this;
+        this.store = new MemoryStore(function() {
+            self.showAlert('Store Initialized', 'Info');
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
